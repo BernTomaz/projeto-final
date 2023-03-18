8290002d83ad3ef6981686999a01af7cf0fc2534
Bernardo Tomaz <bernardo.tomaz@hotmail.com>
2023-03-18 15:37:35 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function ativarPergunta(event) {
 
   resposta.classList.toggle("ativa");
   const ativa = resposta.classList.contains("ativa");
-  pergunta.setAttribute("aria-expanded", "true");
+  pergunta.setAttribute("aria-expanded", ativa);
   console.log(resposta);
 }
 
@@ -43,3 +43,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img ");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 936px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(img);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
